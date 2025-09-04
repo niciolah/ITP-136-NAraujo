39ddd785ab3055e7d1c1296f2df8aa3b5323f674
root <root@DESKTOP-UFBSQ7N>
2025-09-04 18:06:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string patientName;
+        Console.WriteLine("who is being seen today");
+        patientName = Console.ReadLine();
+        Console.WriteLine("welcome: " + patientName);
+    }
+}
